e5dcc47fc136c61ce9fee0d913f20e67f5d5f8a0
mike <mike@win7>
2010-12-16 20:55:47 +0000

    test

--- testcall.php
+++ testcall.php
@@ -2,7 +2,7 @@
     require_once 'lib/instamapper.class.php';
     require_once 'lib/importdata.class.php';
 
-    $insta = new InstaMapper('3141693500957916278');
+    $insta = new InstaMapper('[yourdevicekey]');
     $importer = new ImportData();
     $timestamp = strtotime('2010-08-31 07:30');
     $importer->write($insta->getPoints(100, $timestamp));
--- testweather.php
+++ testweather.php
@@ -1,7 +1,6 @@
 <?php
-    $content = file_get_contents('http://www.worldweatheronline.com/feed/weather.ashx?key=9b1368801d220444100709&q=55.9439,-3.20383&fx=no&format=json');
+    $content = file_get_contents('http://www.worldweatheronline.com/feed/weather.ashx?key=[youweatherkey]&q=55.9439,-3.20383&fx=no&format=json');
 ?><pre><?php
     print_r(json_decode($content));
-
 ?>
 </pre>
